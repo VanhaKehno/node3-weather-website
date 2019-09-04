68c710c91b28eb0d9442541da2cb5d8808ae3e91
Mikko Torniainen <torniainen.mikko@gmail.com>
2019-09-04 13:20:58 -0700

    Use static webpage

--- src/app.js
+++ src/app.js
@@ -1,26 +1,37 @@
+const path = require('path')
 const express = require('express')
+
+console.log(__dirname)
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
 
+app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
-  res.send('Hello there!')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page')
+  res.send([{
+    name: 'Mikko',
+    age: 39
+  }, {
+    name: 'Pertti',
+  }])
 })
 
 app.get('/about', (req, res) => {
-  res.send('About')
+  res.send('<title>About</title><h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send("Current Weather")
+  res.send({
+    location:'Muurame',
+    weather: "Rainy as fuck"
+  })
 })
-// app.com
-// app.com/help
-// app.com/about
+
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
