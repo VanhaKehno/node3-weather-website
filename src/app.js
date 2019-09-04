3c0e501fc3d8d0c2b20b5cea3d3a09900a141105
Mikko Torniainen <torniainen.mikko@gmail.com>
2019-09-04 13:26:55 -0700

    Add about and help pages

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
 const publicDirectoryPath = path.join(__dirname, '../public')
+console.log(publicDirectoryPath)
 
 const app = express()
 
@@ -10,19 +10,6 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
   res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-  res.send([{
-    name: 'Mikko',
-    age: 39
-  }, {
-    name: 'Pertti',
-  }])
-})
-
-app.get('/about', (req, res) => {
-  res.send('<title>About</title><h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
