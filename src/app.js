3bbf307b622d8e4c52c9ebafaa7df00b82406897
Mikko Torniainen <torniainen.mikko@gmail.com>
2019-09-04 14:38:36 -0700

    Use handlebars partials

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -23,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    help: 'This is help text'
+    help: 'This is help text',
+    name: 'Mikko Torniainen',
+    title: 'Help'
   })
 })
 
